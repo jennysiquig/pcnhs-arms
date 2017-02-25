004a223af300db2eef50ad38adb6db4908932f24
Kyle Halog <aoschleger@gmail.com>
2017-02-26 01:51:19 +0800

    fixed verify.php

--- resources/verify.php
+++ resources/verify.php
@@ -6,13 +6,13 @@
 	/*   */
 	$username = $_POST['username'];
 	$password = $_POST['password'];
-	
+
 	$queryStatement = "";
 	/*   */
 	if(!$conn) {
 		die("Connection failed: " . mysqli_connect_error());
 	}
-		$queryStatement = "SELECT * from personnel where uname = ? and password = ?";
+	$queryStatement = "SELECT * from personnel where uname = ? and password = ? and accnt_status = 'ACTIVE'";
 	
 	$preparedQuery = $conn->prepare($queryStatement);
 	$preparedQuery->bind_param("ss",$username,$password);
@@ -21,34 +21,33 @@
 	/*   */
 	if($result->num_rows>0) {
 		while ($row=$result->fetch_assoc()) {
-			if($row['access_type']=="system administrator") {
+			if($row['access_type']=="SYSTEM ADMINISTRATOR") {
 				$_SESSION['username'] = $row['username'];
 				$_SESSION['first_name'] = $row['first_name'];
 				$_SESSION['last_name'] = $row['last_name'];
-				// 
+				//
 				$_SESSION['account_type'] = "systemadmin";
 				$_SESSION['logged_in'] = "true";
-				// 
+                $_SESSION['accnt_status'] = "ACTIVE";
+				//
 				header("Location: ../systemadmin/index.php");
 			}
-			if($row['access_type']=="registrar") {
+			if($row['access_type']=="REGISTRAR") {
 				$_SESSION['username'] = $row['username'];
 				$_SESSION['first_name'] = $row['first_name'];
 				$_SESSION['last_name'] = $row['last_name'];
-				// 
+				//
+                $_SESSION['account_type'] = "registrar";
 				$_SESSION['logged_in'] = "true";
-				$_SESSION['account_type'] = "registrar";
-				// 
-
+                $_SESSION['accnt_status'] = "ACTIVE";
+				//
 				header("Location: ../registrar/index.php");
 			}
-			
-			/*   */
-			
 		}
-	}else {
-		$_SESSION['error_message'] = "Invalid Username or Password";
-
+	}
+	else {
+        $_SESSION['error_message'] = "INVALID LOGIN <br>
+									  CONTACT SYSTEM ADMIN";
 		die(header("Location: ../login.php"));
 	}
 	$conn->close();
