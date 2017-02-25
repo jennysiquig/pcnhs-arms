3ab169a080f2411a325b09d0874baadc9fc8eba1
DESKTOP-A45F7T5\Gilfoyle Luci4 <ashburnaguilan@gmail.com>
2017-02-25 21:54:01 +0800

    Final PHP scipts for deleting or updating (edit) of a personnel account

---
+++ systemadmin/personnelmanagement/phpupdate/personnel_update_info.php
@@ -0,0 +1,22 @@
+<?php
+    require_once "../../../resources/config.php";
+
+    if(!$conn) {
+        die();
+    }
+
+    $per_id = $_POST['per_id'];
+    $uname = $_POST['uname'];
+    $password = $_POST['password'];
+    $last_name = $_POST['last_name'];
+    $first_name = $_POST['first_name'];
+    $mname = $_POST['mname'];
+    $position = $_POST['position'];
+    $access_type = $_POST['access_type'];
+    $accnt_status = $_POST['accnt_status'];;
+
+    $updatestmnt = "UPDATE `pcnhsdb`.`personnel` SET `uname`='$uname', `password`='$password', `last_name`='$last_name', `first_name`='$first_name', `mname`='$mname', `position`='$position', `access_type` = '$access_type',`accnt_status`='$accnt_status' where personnel.per_id = '$per_id'";
+
+    mysqli_query($conn, $updatestmnt);
+    header("location: ../personnel_view.php?per_id=$per_id");
+?>
