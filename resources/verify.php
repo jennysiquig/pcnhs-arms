42923512be6ddfbc15f57049b5c60568bcff801c
DESKTOP-A45F7T5\Gilfoyle Luci4 <ashburnaguilan@gmail.com>
2017-03-02 01:00:35 +0800

    Added some sessions for capturing user login details

--- resources/verify.php
+++ resources/verify.php
@@ -1,54 +1,70 @@
 <?php
-	session_start();
-?>
-<?php
-	require_once 'config.php';
-	/*   */
-	$username = $_POST['username'];
-	$password = $_POST['password'];
+    session_start();
 
-	$queryStatement = "";
-	/*   */
-	if(!$conn) {
-		die("Connection failed: " . mysqli_connect_error());
-	}
-	$queryStatement = "SELECT * from personnel where uname = ? and password = ? and accnt_status = 'ACTIVE'";
-	
-	$preparedQuery = $conn->prepare($queryStatement);
-	$preparedQuery->bind_param("ss",$username,$password);
-	$preparedQuery->execute();
-	$result = $preparedQuery->get_result();
-	/*   */
-	if($result->num_rows>0) {
-		while ($row=$result->fetch_assoc()) {
-			if($row['access_type']=="SYSTEM ADMINISTRATOR") {
-				$_SESSION['username'] = $row['username'];
-				$_SESSION['first_name'] = $row['first_name'];
-				$_SESSION['last_name'] = $row['last_name'];
-				//
-				$_SESSION['account_type'] = "systemadmin";
-				$_SESSION['logged_in'] = "true";
+    require_once 'config.php';
+    /*   */
+    date_default_timezone_set('Asia/Manila');
+    $_SESSION['sDate'] = date("d/m/Y");
+    $_SESSION['liTime'] = date("h:i:sa");
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    $queryStatement = "";
+    /*   */
+    if(!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+    $queryStatement = "SELECT * from personnel where uname = ? and password = ? and accnt_status = 'ACTIVE'";
+
+    $preparedQuery = $conn->prepare($queryStatement);
+    $preparedQuery->bind_param("ss",$username,$password);
+    $preparedQuery->execute();
+    $result = $preparedQuery->get_result();
+
+    $log_id = null;
+    $_SESSION['log_id'] = $log_id;
+    //$_SESSION['username'] = $username;
+
+/*   */
+    if($result->num_rows>0) {
+        while ($row=$result->fetch_assoc()) {
+            if($row['access_type']=="SYSTEM ADMINISTRATOR" || $row['access_type']=="system administrator") {
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['first_name'] = $row['first_name'];
+                $_SESSION['last_name'] = $row['last_name'];
+                //
+                $_SESSION['account_type'] = "systemadmin";
+                $_SESSION['logged_in'] = "true";
                 $_SESSION['accnt_status'] = "ACTIVE";
-				//
-				header("Location: ../systemadmin/index.php");
-			}
-			if($row['access_type']=="REGISTRAR") {
-				$_SESSION['username'] = $row['username'];
-				$_SESSION['first_name'] = $row['first_name'];
-				$_SESSION['last_name'] = $row['last_name'];
-				//
+
+                //For User_Logs
+                $_SESSION['username'] = $username;
+                $_SESSION['accnt_type'] = "SYSTEM ADMIN";
+
+                header("Location: ../systemadmin/index.php");
+            }
+            if($row['access_type']=="REGISTRAR" || $row['access_type']=="registrar") {
+                $_SESSION['username'] = $row['username'];
+                $_SESSION['first_name'] = $row['first_name'];
+                $_SESSION['last_name'] = $row['last_name'];
+                //
                 $_SESSION['account_type'] = "registrar";
-				$_SESSION['logged_in'] = "true";
+                $_SESSION['logged_in'] = "true";
                 $_SESSION['accnt_status'] = "ACTIVE";
-				//
-				header("Location: ../registrar/index.php");
-			}
-		}
-	}
-	else {
+
+                //For User_Logs
+                $_SESSION['username'] = $username;
+                $_SESSION['accnt_type'] = "REGISTRAR";
+
+                header("Location: ../registrar/index.php");
+            }
+        }
+    }
+    else {
         $_SESSION['error_message'] = "INVALID LOGIN <br>
-									  CONTACT SYSTEM ADMIN";
-		die(header("Location: ../login.php"));
-	}
-	$conn->close();
+                                          CONTACT SYSTEM ADMIN";
+        die(header("Location: ../login.php"));
+    }
+    $conn->close();
 ?>
