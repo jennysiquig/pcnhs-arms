dbe63c1ce4c564fc74b61e6411fc0ea3c39a3213
DESKTOP-A45F7T5\Gilfoyle Luci4 <ashburnaguilan@gmail.com>
2017-03-29 23:41:47 +0800

    Updated user activity logs, returns notification "no result".

--- systemadmin/index.php
+++ systemadmin/index.php
@@ -20,72 +20,59 @@
     $_SESSION['last_activity'] = $time;
   ?>
 <html>
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        <!-- Bootstrap -->
+        <link href="../resources/libraries/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+        <!-- Font Awesome -->
+        <link href="../resources/libraries/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+        <!-- NProgress -->
+        <link href="../resources/libraries/nprogress/nprogress.css" rel="stylesheet">
+        <!-- Datatables -->
+        <link href="../resources/libraries/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+        <!-- Custom Theme Style -->
+        <link href="../css/custom.min.css" rel="stylesheet">
+        <link href="../css/tstheme/style.css" rel="stylesheet">
 
+    </head>
+    <body class="nav-md">
+        <?php include "../resources/templates/admin/sidebar.php"; ?>
+        <?php include "../resources/templates/admin/top-nav.php"; ?>
+        <!-- Content Start -->
+        <div class="right_col" role="main">
+            <form class="form-horizontal form-label-left" action="index.php" method="GET">
 
-    <!-- Bootstrap -->
-    <link href="../resources/libraries/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
-    <!-- Font Awesome -->
-    <link href="../resources/libraries/font-awesome/css/font-awesome.min.css" rel="stylesheet">
-
-    <!-- Datatables -->
-    <link href="../resources/libraries/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom Theme Style -->
-    <link href="../css/custom.min.css" rel="stylesheet">
-    <link href="../css/tstheme/style.css" rel="stylesheet">
-
-    <!--[if lt IE 9]>
-    <script src="../js/ie8-responsive-file-warning.js"></script>
-    <![endif]-->
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-</head>
-<body class="nav-md">
-<?php include "../resources/templates/admin/sidebar.php"; ?>
-<?php include "../resources/templates/admin/top-nav.php"; ?>
-<!-- Content Start -->
-<div class="right_col" role="main">
-    <form class="form-horizontal form-label-left" action="index.php" method="GET">
-
-        <div class="form-group">
-            <div class="col-sm-5"></div>
-            <div class="col-sm-7">
-                <div class="input-group">
-
-                    <input type="text" class="form-control" name="search_key" placeholder="Search Personnel Username">
-                    <span class="input-group-btn">
-                  <button class="btn btn-primary">Go</button>
-                </span>
+                 <div class="form-group">
+                 <div class="col-sm-5"></div>
+                    <div class="col-sm-7">
+                        <div class="input-group">
+                            <input type="text" class="form-control" name="search_key" placeholder="Search Personnel Username">
+                                <span class="input-group-btn">
+                                       <button class="btn btn-primary">Go</button>
+                                </span>
+                        </div>
+                    </div>
                 </div>
-            </div>
-        </div>
-    </form>
-    <div class="clearfix"></div>
-    <div class="">
+            </form>
+            <div class="clearfix"></div>
+            <div class="">
 
-        <div class="clearfix"></div>
-        <div class="col-md-12 col-sm-12 col-xs-12">
-            <div class="x_panel">
-                <div class="x_title">
-                    <h2><i class="fa fa-tasks"> </i> PCNHS-ARMS User Activity Logs</h2>
-                    <div class="clearfix"></div>
-                    <br/>
-
-                </div>
-                <div class="x_content">
-                    <div class="table-responsive">
-                        <table id="logList" class="table table-bordered tablesorter">
-                            <thead>
+            <div class="clearfix"></div>
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="x_panel">
+                        <div class="x_title">
+                            <h2><i class="fa fa-tasks"> </i> PCNHS-ARMS User Activity Logs</h2>
+                            <div class="clearfix"></div>
+                            <br/>
+                         </div>
+            <div class="x_content">
+                <div class="table-responsive">
+                    <table id="logList" class="table table-bordered tablesorter">
+                        <thead>
                             <tr>
                                 <th>Log ID</th>
                                 <th>Date</th>
@@ -124,8 +111,14 @@
                             }
 
                             $result = $conn->query($statement);
-                            if ($result->num_rows > 0) {
-                                // output data of each row
+                            if ($result ->num_rows == 0) {
+                                echo <<<NORES
+                                    <tr class="odd pointer">
+                                    <span class="badge badge-danger">NO RESULT</span>        
+                                    </tr>
+NORES;
+                            }
+                            else if ($result->num_rows > 0) {
                                 while($row = $result->fetch_assoc()) {
                                     $log_id = $row['log_id'];
                                     $log_date = $row['log_date'];
@@ -138,14 +131,14 @@
 
                                     echo <<<LOGLIST
                                             <tr class="odd pointer">
-														<td class=" ">$log_id</td>
-														<td class=" ">$log_date</td>
-														<td class=" ">$user_name</td>
-														<td class=" ">$account_type</td>
-														<td class=" ">$log_in_time</td>
+                                                        <td class=" ">$log_id</td>
+                                                        <td class=" ">$log_date</td>
+                                                        <td class=" ">$user_name</td>
+                                                        <td class=" ">$account_type</td>
+                                                        <td class=" ">$log_in_time</td>
                                                         <td class=" ">$user_act</td>
-														<td class=" ">$log_out_time</td>													
-											</tr>
+                                                        <td class=" ">$log_out_time</td>                                                    
+                                            </tr>
 LOGLIST;
                                         }
                                 }
@@ -155,12 +148,18 @@ LOGLIST;
                         
                         <?php
                         $statement = "SELECT * FROM pcnhsdb.user_logs";
-                        $rows = mysqli_num_rows(mysqli_query($conn, $statement));
-                        $total = ceil($rows/$limit);
-
+                        $resultLogs = $conn->query($statement);
+                        if ($resultLogs->num_rows > 5000) {
+                            $queryLogs = "TRUNCATE `pcnhsdb`.`user_logs`";
+                            //TO DO Back Up Table
+                        }
+                        else{
+                            $rows = mysqli_num_rows(mysqli_query($conn, $statement));
+                            $total = ceil($rows/$limit);
+                        }
                         echo '<div class="pull-right">
-                      <div class="col s12">
-                      <ul class="pagination center-align">';
+                                <div class="col s12">
+                                <ul class="pagination center-align">';
                         if($page > 1) {
                             echo "<li class=''><a href='index.php?page=".($page-1)."'>Previous</a></li>";
                         }else if($total <= 0) {
@@ -184,39 +183,38 @@ LOGLIST;
                         }
                         echo "</ul></div></div>";
                         ?>
+
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-</div>
-<!-- Content End -->
-<?php include "../resources/templates/registrar/footer.php"; ?>
+    <!-- Content End -->
+    <?php include "../resources/templates/registrar/footer.php"; ?>
 
-<!-- Scripts -->
-<!-- jQuery -->
-<script src="../resources/libraries/jquery/dist/jquery.min.js" ></script>
-<!-- Bootstrap -->
-<script src="../resources/libraries/bootstrap/dist/js/bootstrap.min.js"></script>
-<!-- FastClick -->
-<script src= "../resources/libraries/fastclick/lib/fastclick.js"></script>
-<!-- input mask -->
-<script src= "../resources/libraries/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
-<script src= "../resources/libraries/parsleyjs/dist/parsley.min.js"></script>
-<!-- Custom Theme Scripts -->
-<script src= "../js/custom.min.js"></script>
+    <!-- Scripts -->
+    <!-- jQuery -->
+    <script src="../resources/libraries/jquery/dist/jquery.min.js" ></script>
+    <!-- Bootstrap -->
+    <script src="../resources/libraries/bootstrap/dist/js/bootstrap.min.js"></script>
+    <!-- FastClick -->
+    <script src= "../resources/libraries/fastclick/lib/fastclick.js"></script>
+    <!-- input mask -->
+    <script src= "../resources/libraries/jquery.inputmask/dist/min/jquery.inputmask.bundle.min.js"></script>
+    <script src= "../resources/libraries/parsleyjs/dist/parsley.min.js"></script>
+    <!-- NProgress -->
+    <script src="../resources/libraries/nprogress/nprogress.js"></script>
+    <!-- Custom Theme Scripts -->
+    <script src= "../js/custom.min.js"></script>
+    <script type="text/javascript" src=<?php echo "../resources/libraries/tablesorter/jquery.tablesorter.js" ?>></script>
+    <!-- Scripts -->
+    <script type="text/javascript">
+        $(document).ready(function(){
+                $("#logList").tablesorter({headers: { 7:{sorter: false}, }});
+            }
+        );
+    </script>
 
-<script type="text/javascript" src=<?php echo "../resources/libraries/tablesorter/jquery.tablesorter.js" ?>></script>
-<!-- Scripts -->
-
-<script type="text/javascript">
-
-    $(document).ready(function(){
-            $("#logList").tablesorter({headers: { 7:{sorter: false}, }});
-        }
-    );
-
-</script>
-
-</body>
+    </body>
 </html>
