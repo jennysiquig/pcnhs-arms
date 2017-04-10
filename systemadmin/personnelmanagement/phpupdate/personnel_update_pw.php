469f78cb3f06e17927c5193272a16844de6fc49a
DESKTOP-A45F7T5\Gilfoyle Luci4 <ashburnaguilan@gmail.com>
2017-04-10 19:09:04 +0800

    Hash values and password verifications included in update, delete and insert scripts.

--- systemadmin/personnelmanagement/phpupdate/personnel_update_pw.php
+++ systemadmin/personnelmanagement/phpupdate/personnel_update_pw.php
@@ -1,33 +1,70 @@
 <?php
     require_once "../../../resources/config.php";
+    require_once "../bcrypt/Bcrypt.php";
+
+    include('../../../resources/classes/Popover.php');
+    
     session_start();
 
     if(!$conn) {
         die();
     }
 
-    $per_id = $_POST['per_id'];
-    $uname = $_POST['uname'];
-    $password = $_POST['password'];
-    $last_name = $_POST['last_name'];
-    $first_name = $_POST['first_name'];
-    $mname = $_POST['mname'];
-    $position = $_POST['position'];
-    $access_type = $_POST['access_type'];
-    $accnt_status = $_POST['accnt_status'];
+    $per_id = htmlspecialchars($_POST['per_id'], ENT_QUOTES);
+    $uname = htmlspecialchars($_POST['uname'], ENT_QUOTES);
+    $password = htmlspecialchars($_POST['password']);
+    $new_password = htmlspecialchars($_POST['new_password']);
+    $hashed_pw = htmlspecialchars($_POST['hashed_pw'], ENT_QUOTES);
+    $last_name = htmlspecialchars($_POST['last_name'], ENT_QUOTES);
+    $first_name = htmlspecialchars($_POST['first_name'], ENT_QUOTES);
+    $mname = htmlspecialchars($_POST['mname'], ENT_QUOTES);
+    $position = htmlspecialchars($_POST['position'], ENT_QUOTES);
+    $access_type = htmlspecialchars($_POST['access_type'], ENT_QUOTES);
+    $accnt_status = htmlspecialchars($_POST['accnt_status'], ENT_QUOTES);
 
-    $query = "SELECT * from personnel where per_id = ?";
+    $pwCheck = "SELECT * from personnel where per_id = ? and password = ?";
+    $preparedPwCheck = $conn->prepare($pwCheck);
+    $preparedPwCheck->bind_param("ss",$per_id,$password);
+    $preparedPwCheck->execute();
+    $resultCheckPw = $preparedPwCheck->get_result();
 
-    $preparedQuery = $conn->prepare($query);
-    $preparedQuery->bind_param("s",$per_id);
-    $preparedQuery->execute();
-    $result = $preparedQuery->get_result();
- 
-    $updatestmnt = "UPDATE `pcnhsdb`.`personnel` 
-                    SET `uname`='$uname', `password`='$password', `last_name`='$last_name', `first_name`='$first_name', `mname`='$mname', `position`='$position', `access_type` = '$access_type',`accnt_status`='$accnt_status' 
-                    WHERE personnel.per_id = '$per_id'";
+    if($resultCheckPw->num_rows>0) {
+        while ($row=$resultCheckPw->fetch_assoc()) {
+        
+         $verifypw = Bcrypt::checkPassword($password, $row['hashed_pw']);
 
-    mysqli_query($conn, $updatestmnt);
+         if($verifypw = true) {
+            
+            $hashed = Bcrypt::hashPassword($password);
+            $updatestmnt = "UPDATE `pcnhsdb`.`personnel` 
+            SET `uname`='$uname', `password`='$new_password',`hashed_pw` = '$hashed', `last_name`='$last_name', `first_name`='$first_name', `mname`='$mname', `position`='$position', `access_type` = '$access_type',`accnt_status`='$accnt_status' 
+            WHERE personnel.per_id = '$per_id'";
+
+            mysqli_query($conn, $updatestmnt);
+
+            $per_edit = "EDITED PERSONNEL ACCOUNT : $per_id";
+            $_SESSION['user_activity'][] = $per_edit;
+
+            //NOTIFICATIONS
+            $alert_type = "info";
+            $message = "Personnel Account Password Updated Successfully";
+            $popover = new Popover();
+            $popover->set_popover($alert_type, $message);   
+            $_SESSION['success_personnel_edit'] = $popover->get_popover();
+            
+            header("location: ../personnel_view.php?per_id=$per_id");  
+            
+            }
+        }
+    }
     
-    header("location: ../personnel_view.php?per_id=$per_id");           
+    else {
+        $alert_type = "danger";
+        $error_message = "Cannot Edit Password: Incorrect Password";
+        $popover = new Popover();
+        $popover->set_popover($alert_type, $error_message);
+        $_SESSION['incorrect_pw_change'] = $popover->get_popover(); 
+        header("location: ../personnel_edit.php?per_id=$per_id");
+                } 
+           
 ?>
