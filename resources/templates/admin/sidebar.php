0c0e337a1805315a2048df37a83fa371eef7b455
Kyle Halog <aoschleger@gmail.com>
2017-04-11 21:07:59 +0800

    Fixed Sidebar menu in System Admin page.

--- resources/templates/admin/sidebar.php
+++ resources/templates/admin/sidebar.php
@@ -9,13 +9,13 @@ echo <<<SB
         <div class="col-md-3 left_col menu_fixed">
           <div class="left_col scroll-view">
             <div class="navbar nav_title" style="border: 0;">
-              <a href="$base_url/systemadmin/index.php" class="site_title"><i class="fa fa-book"></i> <span>PCNHS-ARMS</span></a>
+              <a href="$base_url/systemadmin/index.php" class="site_title"><img src="$base_url/assets/images/pcnhslogo.png" style='height: 40px; width: 40px;'/> <span>PCNHS ARMS</span></a>
             </div>
 
             <div class="clearfix"></div>
 
             <!-- menu profile quick info -->
-            <div class="profile">
+            <div class="profile clearfix">
               <div class="profile_pic">
                 <img src="$base_url/assets/images/icon-user-default.png" alt="admin" class="img-circle profile_img">
               </div>
@@ -26,12 +26,12 @@ echo <<<SB
             </div>
             <!-- /menu profile quick info -->
 
-            <br />
+            <br>
 
             <!-- sidebar menu -->
             <div id="sidebar-menu" class="main_menu_side hidden-print main_menu">
               <div class="menu_section">
-                <h3>General</h3>
+                <h3>Menu</h3>
                 <ul class="nav side-menu">
                 
                   <li><a><i class="fa fa-desktop"></i> System Logs <span class="fa fa-chevron-down"></span></a>
