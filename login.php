2fd4fa8f0a6b7e4b0e078dbc4cc69703e70ceaa8
Kyle Halog <aoschleger@gmail.com>
2017-04-15 16:14:17 +0800

    Undo changes.

--- login.php
+++ login.php
@@ -1,8 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
 <?php
-header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
-header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 date_default_timezone_set('Asia/Manila');
 session_start();
 include_once 'resources/classes/Popover.php';
