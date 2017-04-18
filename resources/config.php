68e9331b19c31e1a9b626d020d31d57173535bac
Kyle Halog <aoschleger@gmail.com>
2017-04-18 13:40:45 +0800

    Default config.

--- resources/config.php
+++ resources/config.php
@@ -1,13 +1,13 @@
 
 <?php
-	 $urlpath = ""; //set path to the folder name of the web application (ex: pcnhs.sis)
-	 $base_url = "https://".$_SERVER['SERVER_NAME'].'/'.$urlpath; //base_url (localhost/pcnhs.sis)
+	 // $urlpath = ""; //set path to the folder name of the web application (ex: pcnhs.sis)
+	 // $base_url = "https://".$_SERVER['SERVER_NAME'].'/'.$urlpath; //base_url (localhost/pcnhs.sis)
 
-	//$urlpath = "pcnhs-arms"; //set path to the folder name of the web application (ex: pcnhs.sis)
-	//$base_url = "http://".$_SERVER['SERVER_NAME'].'/'.$urlpath; //base_url (localhost/pcnhs.sis)
+	$urlpath = "pcnhs-arms"; //set path to the folder name of the web application (ex: pcnhs.sis)
+	$base_url = "http://".$_SERVER['SERVER_NAME'].'/'.$urlpath; //base_url (localhost/pcnhs.sis)
     $host = 'localhost';
     $db = 'pcnhsdb'; //default
-    $user = 'root'; //default
-    $pass = 'root'; //default
+    $user = 'pcnhs'; //default
+    $pass = 'pcnhs'; //default
     $conn = new mysqli($host,$user,$pass,$db);
 ?>
--- resources/templates/registrar/pathconfig.php
+++ resources/templates/registrar/pathconfig.php
@@ -1,7 +1,7 @@
 <?php
-	$path = ""; //if error persists set $path = "/pcnhs-arms";
-	$base_url = "https://".$_SERVER['SERVER_NAME'].$path;
+	// $path = ""; //if error persists set $path = "/pcnhs-arms";
+	// $base_url = "https://".$_SERVER['SERVER_NAME'].$path;
 
-	// $path = "/pcnhs-arms"; //if error persists set $path = "/pcnhs-arms";
-	// $base_url = "http://".$_SERVER['SERVER_NAME'].$path;
+	$path = "/pcnhs-arms"; //if error persists set $path = "/pcnhs-arms";
+	$base_url = "http://".$_SERVER['SERVER_NAME'].$path;
 ?>
